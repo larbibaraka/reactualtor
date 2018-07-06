1c50ea256595db7d7f79d89cb830ad92dd502748
laridev <laridev@gmail.com>
2018-07-06 16:20:48 +0100

    adding basic functionalities

--- src/component/buttons.jsx
+++ src/component/buttons.jsx
@@ -2,38 +2,48 @@ import React, {Component} from 'react';
 
 class Buttons extends Component
 {
+  
+  constructor(props){
+    super(props);
+    this.handleclicks = this.handleclicks.bind(this);
+  }
 
-    render(){
+
+  handleclicks = (event)=>{
+       this.props.Buttonclicked(event.target.value)
+  }
+
+  render(){
         return(
             <div className="buttons">
             <div className="row1">
-              <button className="btn btn-action">C</button>
-              <button className="btn btn-action">+/-</button>
-              <button className="btn btn-action">%</button>
-              <button className="btn btn-calc">÷</button>
+              <button className="btn btn-action" onClick={this.handleclicks} value="c">C</button>
+              <button className="btn btn-action" onClick={this.handleclicks} value="+/-">+/-</button>
+              <button className="btn btn-action" onClick={this.handleclicks} value="%">%</button>
+              <button className="btn btn-calc" onClick={this.handleclicks} value="/">÷</button>
             </div>
             <div className="row2">
-              <button className="btn">7</button>
-              <button className="btn">8</button>
-              <button className="btn">9</button>
-              <button className="btn btn-calc">x</button>
+              <button className="btn" onClick={this.handleclicks} value="7">7</button>
+              <button className="btn" onClick={this.handleclicks} value="8">8</button>
+              <button className="btn" onClick={this.handleclicks} value="9">9</button>
+              <button className="btn btn-calc" onClick={this.handleclicks} value="*">x</button>
             </div>
             <div className="row3">
-              <button className="btn">4</button>
-              <button className="btn">5</button>
-              <button className="btn">6</button>
-              <button className="btn btn-calc">-</button>
+              <button className="btn" onClick={this.handleclicks} value="4">4</button>
+              <button className="btn" onClick={this.handleclicks} value="5">5</button>
+              <button className="btn" onClick={this.handleclicks} value="6">6</button>
+              <button className="btn btn-calc" onClick={this.handleclicks} value="-">-</button>
             </div>
             <div className="row4">
-              <button className="btn">1</button>
-              <button className="btn">2</button>
-              <button className="btn">3</button>
-              <button className="btn btn-calc">+</button>
+              <button className="btn" onClick={this.handleclicks} value="1">1</button>
+              <button className="btn" onClick={this.handleclicks} value="2">2</button>
+              <button className="btn" onClick={this.handleclicks} value="3">3</button>
+              <button className="btn btn-calc" onClick={this.handleclicks} value="+">+</button>
             </div>
             <div className="row5">
-              <button className=" btn-0">0</button>
-              <button className="btn">.</button>
-              <button className="btn btn-calc">=</button>
+              <button className=" btn-0" onClick={this.handleclicks} value="0">0</button>
+              <button className="btn" onClick={this.handleclicks} value=".">.</button>
+              <button className="btn btn-calc" onClick={this.handleclicks} value="=">=</button>
             </div>
           </div>
         )
--- src/component/result.jsx
+++ src/component/result.jsx
@@ -2,21 +2,12 @@ import React, {Component} from 'react';
 
 
 class Result extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            result : this.props.result
-        }
-    }
-
     render(){
         return(
         <div className="result">
-          <p>{this.state.result}</p>
+          <p>{this.props.result}</p>
         </div>
         );
     }
 }
-
 export default Result;
